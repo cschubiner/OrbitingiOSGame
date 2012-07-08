7c9abb0e9dc52f4f61b788ca1f89b5239499b548
alexblickenstaff <alex.blickenstaff@gmail.com>
2012-07-08 00:34:56 -0700

    improved constants further

--- Planet Orbiting iOS Game/Planet Orbiting iOS Game/Constants.h
+++ Planet Orbiting iOS Game/Planet Orbiting iOS Game/Constants.h
@@ -31,10 +31,10 @@ const float gravity = .4;
 const float distToSpawn = 100;
 
 //0 -> 1 == gravity completely dissapears -> gravity doesn't goes away at all
-const float gravityDamepenerDeparture = .1;
+const float gravityDamepenerDeparture = .2;
 
 //0 -> 1 == gravity completely dissapears -> gravity doesn't goes away at all
-const float gravityDamepenerArrival = .2;
+const float gravityDamepenerArrival = .4;
 
 //how quickly gravity goes away after tapping. The bigger the number (closer to 1), the faster it goes away
-const float rateToDecreaseGravity = .005;
+const float rateToDecreaseGravity = .01;
