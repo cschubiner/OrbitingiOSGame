e6648915e4117f858b85a79ab6e493d1085d95b2
Clay Schubiner <cschubiner@gmail.com>
2012-07-09 22:25:34 -0700

    scrollable level editor!

--- Level Creator/Level Creator/Level Creator/Game1.cs
+++ Level Creator/Level Creator/Level Creator/Game1.cs
@@ -27,8 +27,11 @@ namespace Level_Creator
         List<Vector2> posArrayForFrame;
         Vector2 currPosToDraw;
         SpriteFont font;
+        SpriteFont biggerFont;
         string toDisplay;
         float planetScaleSize;
+        KeyboardState lastKeyboardState;
+        Vector2 offset;
         // the zone scale is the planet scale * this number
         float zoneScaleRelativeToPlanet;
         public Game1()
@@ -74,12 +77,15 @@ namespace Level_Creator
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             planetTexture = Content.Load<Texture2D>("PlanetMichael");
-            zoneTexture = Content.Load<Texture2D>("zone");
+            zoneTexture = Content.Load<Texture2D>("zone-hd");
             frameTexture = Content.Load<Texture2D>("iphone box");
             // TODO: use this.Content to load your game content here
             lastMouseState = Mouse.GetState();
+            lastKeyboardState = Keyboard.GetState();
+            offset = Vector2.Zero;
 
             font = Content.Load<SpriteFont>("font");
+            biggerFont = Content.Load<SpriteFont>("biggerFont");
         }
 
         /// <summary>
@@ -101,63 +107,84 @@ namespace Level_Creator
             // Allows the game to exit
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
-
-            MouseState mouseState = Mouse.GetState();
-            if (mouseState.LeftButton == ButtonState.Released && lastMouseState.LeftButton == ButtonState.Pressed)
+            if (this.IsActive)
             {
-                Vector2 pos = new Vector2(mouseState.X, mouseState.Y);
-                posArray.Add(pos);
-            }
+                MouseState mouseState = Mouse.GetState();
+                KeyboardState keyboardState = Keyboard.GetState();
 
-            if (mouseState.RightButton == ButtonState.Released && lastMouseState.RightButton == ButtonState.Pressed)
-            {
-                Vector2 pos = new Vector2(mouseState.X, mouseState.Y);
-                posArrayForFrame.Add(pos);
-            }
+                if (mouseState.LeftButton == ButtonState.Released && lastMouseState.LeftButton == ButtonState.Pressed)
+                {
+                    Vector2 pos = new Vector2(mouseState.X - offset.X, mouseState.Y - offset.Y);
+                    posArray.Add(pos);
+                }
 
+                if (mouseState.RightButton == ButtonState.Released && lastMouseState.RightButton == ButtonState.Pressed)
+                {
+                    Vector2 pos = new Vector2(mouseState.X - offset.X, mouseState.Y - offset.Y);
+                    posArrayForFrame.Add(pos);
+                }
                 currPosToDraw = new Vector2(mouseState.X, mouseState.Y);
 
-
-            if (Keyboard.GetState().IsKeyDown(Keys.C))
-            {
-                string first = "[self CreatePlanetAndZone:";
-                string middle = " yPos:";
-                string end = "];\r\n";
-
-                string toCopy = "";
-                foreach (Vector2 pos in posArray)
+                if (keyboardState.IsKeyDown(Keys.C))
                 {
-                    toCopy += first;
-                    toCopy += pos.X.ToString();
-                    toCopy += middle;
-                    toCopy += (graphics.GraphicsDevice.Viewport.Height - pos.Y).ToString();
-                    toCopy += end;
+                    string first = "[self CreatePlanetAndZone:";
+                    string middle = " yPos:";
+                    string end = "];\r\n";
+
+                    string toCopy = "";
+                    foreach (Vector2 pos in posArray)
+                    {
+                        toCopy += first;
+                        toCopy += pos.X.ToString();
+                        toCopy += middle;
+                        toCopy += (graphics.GraphicsDevice.Viewport.Height - pos.Y).ToString();
+                        toCopy += end;
+                    }
+
+                    StreamWriter textOut = new StreamWriter(new FileStream("output.txt", FileMode.Create, FileAccess.Write));
+                    textOut.WriteLine(toCopy);
+                    textOut.Close();
                 }
 
-                StreamWriter textOut = new StreamWriter(new FileStream("output.txt", FileMode.Create, FileAccess.Write));
-                textOut.WriteLine(toCopy);
-                textOut.Close();
-            }
+                if (mouseState.MiddleButton == ButtonState.Released && lastMouseState.MiddleButton == ButtonState.Pressed)
+                    foreach (Vector2 pos in posArray)
+                    {
+                        if (mouseState.X >= (pos.X + offset.X) - zoneTexture.Width * planetScaleSize * zoneScaleRelativeToPlanet / 2 && mouseState.X <= (pos.X + offset.X) + zoneTexture.Width * planetScaleSize * zoneScaleRelativeToPlanet / 2
+                            && mouseState.Y >= (pos.Y + offset.Y) - zoneTexture.Height * planetScaleSize * zoneScaleRelativeToPlanet / 2 && mouseState.Y <= (pos.Y + offset.Y) + zoneTexture.Height * planetScaleSize * zoneScaleRelativeToPlanet / 2)
+                        {
+                            posArray.Remove(pos);
+                            break;
+                        }
+                    }
 
-            if (mouseState.MiddleButton == ButtonState.Released&&lastMouseState.MiddleButton == ButtonState.Pressed)
-            foreach (Vector2 pos in posArray)
-            {
-                if (mouseState.X >= pos.X - zoneTexture.Width * planetScaleSize * zoneScaleRelativeToPlanet / 2 && mouseState.X <= pos.X + zoneTexture.Width * planetScaleSize * zoneScaleRelativeToPlanet / 2
-                    && mouseState.Y >= pos.Y - zoneTexture.Height * planetScaleSize * zoneScaleRelativeToPlanet / 2 && mouseState.Y <= pos.Y + zoneTexture.Height * planetScaleSize * zoneScaleRelativeToPlanet / 2)
+                if (keyboardState.IsKeyDown(Keys.Z))
+                    zoneScaleRelativeToPlanet += .1f * (mouseState.ScrollWheelValue - lastMouseState.ScrollWheelValue) / 120;
+                else
+                    planetScaleSize += .03f * (mouseState.ScrollWheelValue - lastMouseState.ScrollWheelValue) / 120;
+                toDisplay = "Planet Scale (scroll to change): " + planetScaleSize.ToString() + "   Relative Zone Scale (Hold Z while scrolling): " + zoneScaleRelativeToPlanet.ToString() + "    Mouse Pos: " + (offset.X + mouseState.X).ToString() + ", " + (graphics.GraphicsDevice.Viewport.Height - (offset.Y + mouseState.Y)).ToString();
+
+                int offsetPixels = 200;
+                if (keyboardState.IsKeyDown(Keys.Up) && lastKeyboardState.IsKeyUp(Keys.Up))
                 {
-                    posArray.Remove(pos);
-                    break;
+                    offset.Y += offsetPixels;
                 }
+                if (keyboardState.IsKeyDown(Keys.Down) && lastKeyboardState.IsKeyUp(Keys.Down))
+                {
+                    offset.Y += -offsetPixels;
+                }
+                if (keyboardState.IsKeyDown(Keys.Left) && lastKeyboardState.IsKeyUp(Keys.Left))
+                {
+                    offset.X += offsetPixels;
+                }
+                if (keyboardState.IsKeyDown(Keys.Right) && lastKeyboardState.IsKeyUp(Keys.Right))
+                {
+                    offset.X += -offsetPixels;
+                }
+
+                lastKeyboardState = keyboardState;
+                lastMouseState = mouseState;
+                base.Update(gameTime);
             }
-
-            if (Keyboard.GetState().IsKeyDown(Keys.Z))
-                zoneScaleRelativeToPlanet += .1f * (mouseState.ScrollWheelValue - lastMouseState.ScrollWheelValue) / 120;
-            else
-            planetScaleSize += .03f * (mouseState.ScrollWheelValue-lastMouseState.ScrollWheelValue)/120;
-            toDisplay = "Planet Scale (scroll to change): " + planetScaleSize.ToString() + "   Relative Zone Scale (Hold Z while scrolling): " + zoneScaleRelativeToPlanet.ToString() + "    Mouse Pos: "+ mouseState.X.ToString() + ", " + (graphics.GraphicsDevice.Viewport.Height - mouseState.Y).ToString();
-
-            lastMouseState = mouseState;
-            base.Update(gameTime);
         }
 
         /// <summary>
@@ -169,17 +196,20 @@ namespace Level_Creator
             GraphicsDevice.Clear(Color.Black);
             spriteBatch.Begin();
 
-            spriteBatch.DrawString(font, "'C' outputs code to \"output.txt\" in the exe's directory.       Right click to display iPhone's frame size.       Middle click to delete.\n"+toDisplay, new Vector2(10, 
+            spriteBatch.DrawString(font, "'C' outputs code to \"output.txt\" in the exe's directory.     Right click to display iPhone's frame size.     Middle click to delete.      Arrow keys scroll.\n"+toDisplay, new Vector2(10, 
                 graphics.GraphicsDevice.Viewport.Height - 45), Color.Red);
 
-
+            int index= 0 ;
             foreach (Vector2 pos in posArray)
             {
-                spriteBatch.Draw(zoneTexture, pos, null, Color.White, 0, new Vector2(zoneTexture.Width / 2, zoneTexture.Height / 2), planetScaleSize * zoneScaleRelativeToPlanet, SpriteEffects.None, 0);
-                spriteBatch.Draw(planetTexture, pos, null, Color.White, 0, new Vector2(planetTexture.Width / 2, planetTexture.Height / 2), planetScaleSize, SpriteEffects.None, 0);
+
+                spriteBatch.Draw(zoneTexture, pos+offset, null, Color.White, 0, new Vector2(zoneTexture.Width / 2, zoneTexture.Height / 2), planetScaleSize * zoneScaleRelativeToPlanet, SpriteEffects.None, 0);
+                spriteBatch.Draw(planetTexture, pos + offset, null, Color.White, 0, new Vector2(planetTexture.Width / 2, planetTexture.Height / 2), planetScaleSize, SpriteEffects.None, 0);
+                spriteBatch.DrawString(biggerFont, index.ToString(), pos + offset, new Color(97,255,110));
+                index++;
             }
             foreach (Vector2 pos in posArrayForFrame)
-                spriteBatch.Draw(frameTexture, pos, null, Color.White, 0, new Vector2(frameTexture.Width / 2, frameTexture.Height / 2), 1, SpriteEffects.None, 0);
+                spriteBatch.Draw(frameTexture, pos + offset, null, Color.White, 0, new Vector2(frameTexture.Width / 2, frameTexture.Height / 2), 1, SpriteEffects.None, 0);
             MouseState mouseState = Mouse.GetState();
             if (mouseState.LeftButton == ButtonState.Pressed)
             {
