658ea06eeaa1f12c4beb05b0b14f2e480376e040
Jeff Grimes <jeffgrimes9@gmail.com>
2012-07-21 20:31:20 -0700

    added infrastructure for coin system

---
+++ Planet Orbiting iOS Game/Planet Orbiting iOS Game/UserWallet.h
@@ -0,0 +1,13 @@
+//
+//  UserWallet.h
+//  Planet Orbiting iOS Game
+//
+//  Created by Jeff Grimes on 7/21/12.
+//  Copyright (c) 2012 Stanford University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserWallet : NSObject
+
+@end
---
+++ Planet Orbiting iOS Game/Planet Orbiting iOS Game/UserWallet.m
@@ -0,0 +1,13 @@
+//
+//  UserWallet.m
+//  Planet Orbiting iOS Game
+//
+//  Created by Jeff Grimes on 7/21/12.
+//  Copyright (c) 2012 Stanford University. All rights reserved.
+//
+
+#import "UserWallet.h"
+
+@implementation UserWallet
+
+@end
