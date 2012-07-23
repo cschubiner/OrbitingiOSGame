d6f40a2ae22556aa194ee03037013c554e860635
Alex Blickenstaff <alex@concept.io>
2012-07-22 19:51:14 -0700

    a lot of gravity stuff

---
+++ Planet Orbiting iOS Game/Planet Orbiting iOS Game/Coin.h
@@ -0,0 +1,13 @@
+//
+//  Coin.h
+//  Planet Orbiting iOS Game
+//
+//  Created by Alex Blickenstaff on 7/22/12.
+//  Copyright (c) 2012 Stanford University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Coin : NSObject
+
+@end
---
+++ Planet Orbiting iOS Game/Planet Orbiting iOS Game/Coin.m
@@ -0,0 +1,13 @@
+//
+//  Coin.m
+//  Planet Orbiting iOS Game
+//
+//  Created by Alex Blickenstaff on 7/22/12.
+//  Copyright (c) 2012 Stanford University. All rights reserved.
+//
+
+#import "Coin.h"
+
+@implementation Coin
+
+@end
