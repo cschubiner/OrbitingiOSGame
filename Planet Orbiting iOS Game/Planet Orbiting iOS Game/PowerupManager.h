4066a486dec634f5ae132e9ea5ce4f8dace4b3c5
Jeff Grimes <jeffgrimes9@gmail.com>
2012-07-24 01:55:01 -0700

    Use POWERUPMANAGER for max sexiness, or else I will poop in your mouth :(

---
+++ Planet Orbiting iOS Game/Planet Orbiting iOS Game/PowerupManager.h
@@ -0,0 +1,13 @@
+//
+//  PowerupManager.h
+//  Planet Orbiting iOS Game
+//
+//  Created by Jeff Grimes on 7/24/12.
+//  Copyright (c) 2012 Stanford University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PowerupManager : NSObject
+
+@end
---
+++ Planet Orbiting iOS Game/Planet Orbiting iOS Game/PowerupManager.m
@@ -0,0 +1,13 @@
+//
+//  PowerupManager.m
+//  Planet Orbiting iOS Game
+//
+//  Created by Jeff Grimes on 7/24/12.
+//  Copyright (c) 2012 Stanford University. All rights reserved.
+//
+
+#import "PowerupManager.h"
+
+@implementation PowerupManager
+
+@end
