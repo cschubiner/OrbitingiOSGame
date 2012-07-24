af7d3270fab0b2048c696ea9f97d156e1bf7bf65
Alex Blickenstaff <alex@concept.io>
2012-07-24 00:48:44 -0700

    kjkljklkljj LOLLLLLLLLLLLLL

--- Planet Orbiting iOS Game/Planet Orbiting iOS Game/Constants.h
+++ Planet Orbiting iOS Game/Planet Orbiting iOS Game/Constants.h
@@ -61,7 +61,7 @@ const float minAstVel = 0;
 const float maxAstVel = .4;
 
 //gravity strength when in orbit state 3
-const float freeGravityStrength = 1000;
+const float freeGravityStrength = 700;
 
 //the rate at which gravity increases per update while in orbit state 2/3
 //const float rateToIncreaseGravity = .015;
