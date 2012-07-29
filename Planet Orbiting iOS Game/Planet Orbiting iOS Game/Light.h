17b64eae246e2e2b2669a8dbb176a5c23cd2eed8
Alex Blickenstaff <alex@concept.io>
2012-07-29 01:14:00 -0700

    sjfkldsjf

---
+++ Planet Orbiting iOS Game/Planet Orbiting iOS Game/Light.h
@@ -0,0 +1,13 @@
+//
+//  Light.h
+//  Planet Orbiting iOS Game
+//
+//  Created by Alex Blickenstaff on 7/29/12.
+//  Copyright (c) 2012 Stanford University. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Light : CCNode
+
+@end
---
+++ Planet Orbiting iOS Game/Planet Orbiting iOS Game/Light.m
@@ -0,0 +1,13 @@
+//
+//  Light.m
+//  Planet Orbiting iOS Game
+//
+//  Created by Alex Blickenstaff on 7/29/12.
+//  Copyright (c) 2012 Stanford University. All rights reserved.
+//
+
+#import "Light.h"
+
+@implementation Light
+
+@end
