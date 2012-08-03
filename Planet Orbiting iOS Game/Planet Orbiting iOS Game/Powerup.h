39e8ce88d6ab58a5a7783dbf1c7fb0c31d942f30
Alex Blickenstaff <alex@concept.io>
2012-08-02 23:48:00 -0700

    hjkhjkhlhjklh

---
+++ Planet Orbiting iOS Game/Planet Orbiting iOS Game/Powerup.h
@@ -0,0 +1,13 @@
+//
+//  Powerup.h
+//  Planet Orbiting iOS Game
+//
+//  Created by Alex Blickenstaff on 8/2/12.
+//  Copyright (c) 2012 Stanford University. All rights reserved.
+//
+
+#import "CameraObject.h"
+
+@interface Powerup : CameraObject
+
+@end
---
+++ Planet Orbiting iOS Game/Planet Orbiting iOS Game/Powerup.m
@@ -0,0 +1,13 @@
+//
+//  Powerup.m
+//  Planet Orbiting iOS Game
+//
+//  Created by Alex Blickenstaff on 8/2/12.
+//  Copyright (c) 2012 Stanford University. All rights reserved.
+//
+
+#import "Powerup.h"
+
+@implementation Powerup
+
+@end
