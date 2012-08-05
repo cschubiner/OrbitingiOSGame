3560f519dad28d961e5c60083e0a71cb29f7e2ad
Clay Schubiner <cschubiner@gmail.com>
2012-08-04 23:37:46 -0700

    sdafasdf

--- Planet Orbiting iOS Game/Planet Orbiting iOS Game/GameplayLayer.m
+++ Planet Orbiting iOS Game/Planet Orbiting iOS Game/GameplayLayer.m
@@ -682,6 +682,7 @@ typedef struct {
         id action2 = [CCSequence actions:[CCDelayTime actionWithDuration:.5],[CCSpawn actions:fadeAction,[CCScaleTo actionWithDuration:.3 scale:1.06], nil], nil] ;
         id repeatAction = [CCRepeat actionWithAction:[CCSequence actions:[CCEaseSineInOut actionWithAction:[CCScaleTo actionWithDuration:.8 scale:1.0f]],[CCEaseSineInOut actionWithAction:[CCScaleTo actionWithDuration:.8 scale:1.06f]], nil] times:1];
         galaxyLabelAction = [CCSequence actions:action2,repeatAction, [CCFadeOut actionWithDuration:.8],nil];
+        [galaxyLabelAction retain];
         [galaxyLabel runAction: galaxyLabelAction];
         
         float streakWidth = streakWidthWITHOUTRetinaDisplay;
@@ -1258,9 +1259,10 @@ typedef struct {
         cameraShouldFocusOnPlayer = false;
     }
     else if (targetPlanet.whichGalaxyThisObjectBelongsTo>lastPlanetVisited.whichGalaxyThisObjectBelongsTo) {
-    
+        
         [galaxyLabel setString:[currentGalaxy name]];
-
+        [galaxyLabel stopAllActions];
+        [galaxyLabel runAction:galaxyLabelAction];
         cameraShouldFocusOnPlayer=true;
     }
     else cameraShouldFocusOnPlayer=false;
