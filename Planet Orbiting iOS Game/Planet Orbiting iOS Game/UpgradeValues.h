2cf54447b41e9186066b4f8f4d43cf528cd623a4
Alex Blickenstaff <alex.blickenstaff@gmail.com>
2012-08-07 00:03:40 -0700

    jlskdjfsldkfjsldk

---
+++ Planet Orbiting iOS Game/Planet Orbiting iOS Game/UpgradeValues.h
@@ -0,0 +1,13 @@
+//
+//  UpgradeValues.h
+//  Planet Orbiting iOS Game
+//
+//  Created by Alex Blickenstaff on 8/6/12.
+//  Copyright (c) 2012 Stanford University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UpgradeValues : NSObject
+
+@end
---
+++ Planet Orbiting iOS Game/Planet Orbiting iOS Game/UpgradeValues.m
@@ -0,0 +1,13 @@
+//
+//  UpgradeValues.m
+//  Planet Orbiting iOS Game
+//
+//  Created by Alex Blickenstaff on 8/6/12.
+//  Copyright (c) 2012 Stanford University. All rights reserved.
+//
+
+#import "UpgradeValues.h"
+
+@implementation UpgradeValues
+
+@end
