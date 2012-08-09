e70162e66f07bd86fd1c960f51c6f8eb9f0f8c5a
Alex Blickenstaff <alex.blickenstaff@gmail.com>
2012-08-08 23:56:07 -0700

    jhkjhkjjhk

---
+++ Planet Orbiting iOS Game/Planet Orbiting iOS Game/UpgradeCell.h
@@ -0,0 +1,13 @@
+//
+//  UpgradeCell.h
+//  Planet Orbiting iOS Game
+//
+//  Created by Alex Blickenstaff on 8/8/12.
+//  Copyright (c) 2012 Stanford University. All rights reserved.
+//
+
+#import "CCLayer.h"
+
+@interface UpgradeCell : CCLayer
+
+@end
---
+++ Planet Orbiting iOS Game/Planet Orbiting iOS Game/UpgradeCell.m
@@ -0,0 +1,13 @@
+//
+//  UpgradeCell.m
+//  Planet Orbiting iOS Game
+//
+//  Created by Alex Blickenstaff on 8/8/12.
+//  Copyright (c) 2012 Stanford University. All rights reserved.
+//
+
+#import "UpgradeCell.h"
+
+@implementation UpgradeCell
+
+@end
