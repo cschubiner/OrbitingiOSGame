291746e053bac36c8dcbf36902676f057469f7c4
Alex Blickenstaff <alex.blickenstaff@gmail.com>
2012-08-08 23:02:17 -0700

    threw some stuff into the kitchen sink

---
+++ Planet Orbiting iOS Game/Planet Orbiting iOS Game/UpgradeManager.h
@@ -0,0 +1,13 @@
+//
+//  UpgradeManager.h
+//  Planet Orbiting iOS Game
+//
+//  Created by Alex Blickenstaff on 8/8/12.
+//  Copyright (c) 2012 Stanford University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UpgradeManager : NSObject
+
+@end
