8a7f4b5e650f7985de1a6236ee4bef32edb09f45
Clay Schubiner <cschubiner@gmail.com>
2012-08-13 17:04:26 -0700

    converted to arc

---
+++ Star Dash/Star Dash/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  Star Dash
+//
+//  Created by Clayton Schubiner on 8/13/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
