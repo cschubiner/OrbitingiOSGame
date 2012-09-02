2f2fb9da2ca93108bdd4eac2f4f8b49b1628a0aa
Clay Schubiner <cschubiner@gmail.com>
2012-09-01 20:56:18 -0700

    Revert "delegate"
    This reverts commit 1391eb05ae9d8922204bff9e293efc9fa5171ed0.

--- Planet Orbiting iOS Game/Planet Orbiting iOS Game/AppDelegate.m
+++ Planet Orbiting iOS Game/Planet Orbiting iOS Game/AppDelegate.m
@@ -1,25 +1,32 @@
 //
-// AppDelegate.m
-// Star Dash
+//  AppDelegate.m
+//  Star Dash
 //
-// Created by Clayton Schubiner on 8/13/12.
-// Copyright __MyCompanyName__ 2012. All rights reserved.
+//  Created by Clayton Schubiner on 8/13/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
 //
+
 #import "cocos2d.h"
+
 #import "AppDelegate.h"
 #import "DataStorage.h"
 #import "GameplayLayer.h"
 #import "DataStorage.h"
 #import "IntroLayer.h"
 #import "PlayerStats.h"
+
 @implementation AppDelegate
+
 @synthesize window=window_, navController=navController_, director=director_;
+
 -(bool)getShouldPlayMenuMusic {
     return shouldPlayMenuMusic;
 }
+
 -(void)setShouldPlayMenuMusic:(bool)a_shouldPlay {
     shouldPlayMenuMusic = a_shouldPlay;
 }
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // installs HandleExceptions as the Uncaught Exception Handler
@@ -40,10 +47,11 @@
     [Flurry startSession:@"96GKYS7HQZHNKZJJN2CZ"];
     [Flurry setUserID:[[UIDevice currentDevice] uniqueIdentifier]];
     
-    // [Kamcord setDeveloperKey:@"d05f73399ff3c1755bd97ec94cb5fdda"
-    // developerSecret:@"prcU7MltdajQ1YVTSeFDtPtywe2zABOmzzpSB5pGP79"
-    // appName:@"Star Dash"];
+//    [Kamcord setDeveloperKey:@"d05f73399ff3c1755bd97ec94cb5fdda"
+  //           developerSecret:@"prcU7MltdajQ1YVTSeFDtPtywe2zABOmzzpSB5pGP79"
+    //                 appName:@"Star Dash"];
     
+
     [TestFlight setDeviceIdentifier:[[UIDevice currentDevice] uniqueIdentifier]];
     
     //[[iRate sharedInstance]setDebug:YES];
@@ -55,75 +63,80 @@
     
     shouldPlayMenuMusic = true;
     
-    // Create the main window
-    window_ = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+	// Create the main window
+	window_ = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
-    // Create an CCGLView with a RGB565 color buffer, and a depth buffer of 0-bits
-    CCGLView *glView = [CCGLView viewWithFrame:[window_ bounds]
-                                   pixelFormat:kEAGLColorFormatRGB565//kEAGLColorFormatRGBA8
-                                   depthFormat:0//GL_DEPTH_COMPONENT24_OES
-                            preserveBackbuffer:NO
-                                    sharegroup:nil
-                                 multiSampling:NO
-                               numberOfSamples:0];
+	// Create an CCGLView with a RGB565 color buffer, and a depth buffer of 0-bits
+	CCGLView *glView = [CCGLView viewWithFrame:[window_ bounds]
+								   pixelFormat:kEAGLColorFormatRGB565	//kEAGLColorFormatRGBA8
+								   depthFormat:0	//GL_DEPTH_COMPONENT24_OES
+							preserveBackbuffer:NO
+									sharegroup:nil
+								 multiSampling:NO
+							   numberOfSamples:0];
     
-    director_ = (CCDirectorIOS*) [CCDirector sharedDirector];
+	director_ = (CCDirectorIOS*) [CCDirector sharedDirector];
     
-    director_.wantsFullScreenLayout = YES;
+	director_.wantsFullScreenLayout = YES;
     
-    // Display FSP and SPF
-    [director_ setDisplayStats:YES];
+	// Display FSP and SPF
+	[director_ setDisplayStats:YES];
     
-    // set FPS at 60
-    [director_ setAnimationInterval:1.0/50.0f];
+	// set FPS at 60
+	[director_ setAnimationInterval:1.0/50.0f];
     
-    // attach the openglView to the director
-    [director_ setView:glView];
+	// attach the openglView to the director
+	[director_ setView:glView];
     
-    // for rotation and other messages
-    [director_ setDelegate:self];
+	// for rotation and other messages
+	[director_ setDelegate:self];
     
-    // 2D projection
-    [director_ setProjection:kCCDirectorProjection2D];
-    //[director setProjection:kCCDirectorProjection3D];
+	// 2D projection
+	[director_ setProjection:kCCDirectorProjection2D];
+    //	[director setProjection:kCCDirectorProjection3D];
     
-    // Enables High Res mode (Retina Display) on iPhone 4 and maintains low res on all other devices
-    if( ! [director_ enableRetinaDisplay:YES] )
-        CCLOG(@"Retina Display Not supported");
+	// Enables High Res mode (Retina Display) on iPhone 4 and maintains low res on all other devices
+	if( ! [director_ enableRetinaDisplay:YES] )
+		CCLOG(@"Retina Display Not supported");
     
-    // Default texture format for PNG/BMP/TIFF/JPEG/GIF images
-    // It can be RGBA8888, RGBA4444, RGB5_A1, RGB565
-    // You can change anytime.
-    [CCTexture2D setDefaultAlphaPixelFormat:kCCTexture2DPixelFormat_RGBA8888];
+	// Default texture format for PNG/BMP/TIFF/JPEG/GIF images
+	// It can be RGBA8888, RGBA4444, RGB5_A1, RGB565
+	// You can change anytime.
+	[CCTexture2D setDefaultAlphaPixelFormat:kCCTexture2DPixelFormat_RGBA8888];
     
-    // If the 1st suffix is not found and if fallback is enabled then fallback suffixes are going to searched. If none is found, it will try with the name without suffix.
-    // On iPad HD : "-ipadhd", "-ipad", "-hd"
-    // On iPad : "-ipad", "-hd"
-    // On iPhone HD: "-hd"
-    CCFileUtils *sharedFileUtils = [CCFileUtils sharedFileUtils];
-    [sharedFileUtils setEnableFallbackSuffixes:NO];// Default: NO. No fallback suffixes are going to be used
-    [sharedFileUtils setiPhoneRetinaDisplaySuffix:@"-hd"];// Default on iPhone RetinaDisplay is "-hd"
-    [sharedFileUtils setiPadSuffix:@"-ipad"];// Default on iPad is "ipad"
-    [sharedFileUtils setiPadRetinaDisplaySuffix:@"-ipadhd"];// Default on iPad RetinaDisplay is "-ipadhd"
+	// If the 1st suffix is not found and if fallback is enabled then fallback suffixes are going to searched. If none is found, it will try with the name without suffix.
+	// On iPad HD  : "-ipadhd", "-ipad",  "-hd"
+	// On iPad     : "-ipad", "-hd"
+	// On iPhone HD: "-hd"
+	CCFileUtils *sharedFileUtils = [CCFileUtils sharedFileUtils];
+	[sharedFileUtils setEnableFallbackSuffixes:NO];				// Default: NO. No fallback suffixes are going to be used
+	[sharedFileUtils setiPhoneRetinaDisplaySuffix:@"-hd"];		// Default on iPhone RetinaDisplay is "-hd"
+	[sharedFileUtils setiPadSuffix:@"-ipad"];					// Default on iPad is "ipad"
+	[sharedFileUtils setiPadRetinaDisplaySuffix:@"-ipadhd"];	// Default on iPad RetinaDisplay is "-ipadhd"
     
-    // Assume that PVR images have premultiplied alpha
-    [CCTexture2D PVRImagesHavePremultipliedAlpha:YES];
+	// Assume that PVR images have premultiplied alpha
+	[CCTexture2D PVRImagesHavePremultipliedAlpha:YES];
     
     [DataStorage fetchData];
     
-    // and add the scene to the stack. The director will run it when it automatically when the view is displayed.
-    [director_ pushScene: [IntroLayer scene]];
+	// and add the scene to the stack. The director will run it when it automatically when the view is displayed.
+	[director_ pushScene: [IntroLayer scene]];
     
-    // Create a Navigation Controller with the Director
-    navController_ = [[UINavigationController alloc] initWithRootViewController:director_];
-    navController_.navigationBarHidden = YES;
-    // set the Navigation Controller as the root view controller
-    //[window_ addSubview:navController_.view];// Generates flicker.
-    [window_ setRootViewController:navController_];
-    // make main window visible
-    [window_ makeKeyAndVisible];
-    return YES;
+	
+	// Create a Navigation Controller with the Director
+	navController_ = [[UINavigationController alloc] initWithRootViewController:director_];
+	navController_.navigationBarHidden = YES;
+	
+	// set the Navigation Controller as the root view controller
+    //	[window_ addSubview:navController_.view];	// Generates flicker.
+	[window_ setRootViewController:navController_];
+	
+	// make main window visible
+	[window_ makeKeyAndVisible];
+	
+	return YES;
 }
+
 /*
  My Apps Custom uncaught exception catcher, we do special stuff here, and TestFlight takes care of the rest
  **/
@@ -138,67 +151,86 @@ void SignalHandler(int sig) {
     NSLog(@"This is where we save the application data during a signal");
     // Save application data on crash
 }
+
 // Supported orientations: Landscape. Customize it for your own needs
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    return UIInterfaceOrientationIsLandscape(interfaceOrientation);
+	return UIInterfaceOrientationIsLandscape(interfaceOrientation);
 }
+
 -(UIViewController*)getViewController{
     return navController_;
 }
+
+
+
 -(int)getGalaxyCounter {
     return galaxyCounter;
 }
+
 -(bool)getIsRetinaDisplay {
     return isRetinaDisplay;
 }
+
 -(bool)getIsInTutorialMode{
     return isInTutorialMode;
 }
+
+
 -(void)setIsInTutorialMode:(bool)mode {
     isInTutorialMode=mode;
 }
+
+
 -(void)setGalaxyCounter:(int)count {
     galaxyCounter = count;
 }
+
+
 -(bool)getWasJustBackgrounded {
     return wasJustBackgrounded;
 }
+
 -(void)setWasJustBackgrounded:(bool)isItBackgrounded{
     wasJustBackgrounded = isItBackgrounded;
 }
+
 -(void)setChosenLevelNumber:(int)num {
     chosenLevelNumber = num;
 }
+
 -(int)getChosenLevelNumber {
     return chosenLevelNumber;
 }
+
 // getting a call, pause the game
 -(void) applicationWillResignActive:(UIApplication *)application
 {
-    if( [navController_ visibleViewController] == director_ )
-        [director_ pause];
+	if( [navController_ visibleViewController] == director_ )
+		[director_ pause];
 }
+
 // call got rejected
 -(void) applicationDidBecomeActive:(UIApplication *)application
 {
-    if( [navController_ visibleViewController] == director_ )
-        [director_ resume];
+	if( [navController_ visibleViewController] == director_ )
+		[director_ resume];
 }
+
 -(void) applicationDidEnterBackground:(UIApplication*)application
 {
-    //[Flurry logEvent:@"Application Backgrounded" withParameters:[self getDictionaryOfFlurryParameters]];
-    if( [navController_ visibleViewController] == director_ )
-        [director_ stopAnimation];
-    
-    wasJustBackgrounded = true;
+        [Flurry logEvent:@"Application Backgrounded" withParameters:[self getDictionaryOfFlurryParameters]];
+	if( [navController_ visibleViewController] == director_ )
+		[director_ stopAnimation];
 }
+
 -(void) applicationWillEnterForeground:(UIApplication*)application
 {
     [Flurry logEvent:@"Application entered foreground"];
-    if( [navController_ visibleViewController] == director_ )
-        [director_ startAnimation];
+	if( [navController_ visibleViewController] == director_ )
+		[director_ startAnimation];
 }
+
 - (NSMutableDictionary *)getDictionaryOfFlurryParameters
 {
     NSMutableArray *highScores = [[PlayerStats sharedInstance] getScores];
@@ -209,7 +241,7 @@ void SignalHandler(int sig) {
         NSString *scoreInt = [NSString stringWithFormat:@"%d", [highscoreObject intValue]];
         NSString *scoreName = [keyValuePairs valueForKey:scoreInt ];
         if (!scoreName)
-            [parameterDict addEntriesFromDictionary:[NSDictionary dictionaryWithObjectsAndKeys:highscoreObject,scoreName, nil]];
+        [parameterDict addEntriesFromDictionary:[NSDictionary dictionaryWithObjectsAndKeys:highscoreObject,scoreName, nil]];
     }
     [parameterDict addEntriesFromDictionary:[NSDictionary dictionaryWithObjectsAndKeys:
                                              [NSNumber numberWithInt:[[UserWallet sharedInstance] getBalance]],@"Coin Balance",
@@ -218,23 +250,27 @@ void SignalHandler(int sig) {
                                              nil]];
     return parameterDict;
 }
+
 // application will be killed
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     [Flurry logEvent:@"Application Terminated" withParameters:[self getDictionaryOfFlurryParameters]];
-    [DataStorage storeData];
-    CC_DIRECTOR_END();
+    	[DataStorage storeData];
+	CC_DIRECTOR_END();
 }
+
 // purge memory
 - (void)applicationDidReceiveMemoryWarning:(UIApplication *)application
 {
-    [[CCDirector sharedDirector] purgeCachedData];
+	[[CCDirector sharedDirector] purgeCachedData];
 }
+
 // next delta time will be zero
 -(void) applicationSignificantTimeChange:(UIApplication *)application
 {
-    [[CCDirector sharedDirector] setNextDeltaTimeZero:YES];
+	[[CCDirector sharedDirector] setNextDeltaTimeZero:YES];
 }
+
 + (void)initialize
 {
     //configure iRate
@@ -242,13 +278,18 @@ void SignalHandler(int sig) {
     [iRate sharedInstance].usesUntilPrompt = 10;
     [iRate sharedInstance].eventsUntilPrompt = 17;
 }
+
 -(void)iRateUserDidAttemptToRateApp {
     [Flurry logEvent:@"iRate user did attempt to rate app" withParameters:[NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithInt:[[PlayerStats sharedInstance] getPlays]],@"Number of total plays", nil]];
 }
+
 -(void)iRateUserDidDeclineToRateApp {
     [Flurry logEvent:@"iRate user did decline to rate app" withParameters:[NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithInt:[[PlayerStats sharedInstance] getPlays]],@"Number of total plays", nil]];
 }
+
 -(void)iRateUserDidRequestReminderToRateApp {
     [Flurry logEvent:@"iRate user did request reminder to rate app" withParameters:[NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithInt:[[PlayerStats sharedInstance] getPlays]],@"Number of total plays", nil]];
 }
+
 @end
+
